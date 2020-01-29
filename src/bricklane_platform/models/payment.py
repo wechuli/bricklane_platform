4b6c894c06ef17b65a72e4be4c3c8ca0d0094c1e
Wechuli <wechulipaul@yahoo.com>
2020-01-29 20:51:40 +0300

    edit payment class to know source of payment and process accordingly

--- src/bricklane_platform/models/payment.py
+++ src/bricklane_platform/models/payment.py
@@ -3,6 +3,7 @@ from dateutil.parser import parse
 
 
 from bricklane_platform.models.card import Card
+from bricklane_platform.models.bank_account import BankAccount
 from bricklane_platform.config import PAYMENT_FEE_RATE
 
 
@@ -14,6 +15,7 @@ class Payment(object):
     fee = None
     card_id = None
     bank_account_id = None
+    payment_successful = False
 
     def __init__(self, source, data=None):
 
@@ -32,6 +34,12 @@ class Payment(object):
             card.card_id = int(data["card_id"])
             card.status = data["card_status"]
             self.card = card
+            self.payment_successful = self.card.status == "processed"
+        elif source == "bank":
+            bank_account = BankAccount()
+            bank_account.bank_account_id = int(data["bank_account_id"])
+            self.bank_account = bank_account
+            self.payment_successful = True
 
     def is_successful(self):
-        return self.card.status == "processed"
+        return self.payment_successful
